ce289b2fbd7de9448db194e0edefaba7c780e753
Tiago Bittencourt <tiago.sbittencourt@icloud.com>
2023-05-13 04:00:58 -0300

    CSS adicionado nas palavras quando digitadas corretas ou incorretas

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,27 +5,30 @@ const divParagrafo = document.querySelector('.div-paragraph')
 
 let acertos = 0
 let erros = 0
+let index = 0
 
 const frasePrompt = window.prompt('Escolha uma frase')
 
 let palavraFrase = frasePrompt.split(' ')
 
-document.addEventListener('keypress', function(e) {
+document.addEventListener('keypress', function (e) {
     if (e.key === ' ') {
         checkWord()
+        changeColorWord()
         checkRes()
         clsInput()
     }
 })
 
-window.addEventListener('load', function(e) {
+window.addEventListener('load', function() {
     createLabel()
+    inputPalavra.focus()
 })
 
 function createParagraph() {
     const p = document.createElement('p')
     return p
-}  
+}
 
 function createLabel() {
     for (let i in palavraFrase) {
@@ -41,24 +44,56 @@ function checkWord() {
 
     for (let i in palavraFrase) {
         if (inputPalavra.value === palavraFrase[i]) {
+            changeColorWord(1)
             acertos++
-            palavraFrase.splice(i, 1)  
+            palavraFrase.splice(i, 1)
             console.log('certo')
             break
-        
+
         } else {
-            console.log('errou')
+            changeColorWord(2)
             erros++
-        } 
+            console.log('errou')
+            break
+        }
     }
 }
+
+function changeColorWord(resultado) {
+    const p = document.querySelectorAll('p')
+
+    if (resultado === 1) {
+        for (let i = 0; i < 10000; i++) {
+            if (!p[i].classList.contains('acerto')) {
+                p[i].classList.remove('erro')
+                p[i].classList.add('acerto')
+                console.log(p[i])
+                index = i
+                break
+            }
+        }
+
+    } else if (resultado === 2) {
+        for (let i = index + 1; i < 10000; i++) {
+            if (!p[i].classList.contains('erro')) {
+                p[i].classList.add('erro')
+                console.log(p[i])
+                break
+            }
+        }
+    }
+
+
+}
+
+
 
 function checkRes() {
     if (palavraFrase.length == 0) {
         palavraFrase = frasePrompt.split(' ')
 
-        alert(`voce finalizou o teste! Acertos: ${acertos} Errados: ${erros}`)
-        
+        alert(`voce finalizou o teste! Acertos: ${acertos} Erros: ${erros}`)
+
         acertos = 0
         erros = 0
     }
