b96434307a379195ead3867bdf9f4786f770753f
Tiago Bittencourt <tiago.sbittencourt@icloud.com>
2023-05-13 01:20:40 -0300

    Adicionando opcao de introduzir a frase para o teste

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
+const container = document.querySelector('.container')
 const inputPalavra = document.querySelector('.input-palavra')
 const botao = document.querySelector('.btn-verificar')
-const pFrase = document.querySelector('.p-frase')
-const frase = pFrase.textContent
+const divParagrafo = document.querySelector('.div-paragraph')
 
-let palavraFrase = frase.split(' ')
 let acertos = 0
 let erros = 0
+
+const frasePrompt = window.prompt('Escolha uma frase')
+
+let palavraFrase = frasePrompt.split(' ')
 
 document.addEventListener('keypress', function(e) {
     if (e.key === ' ') {
@@ -15,14 +18,31 @@ document.addEventListener('keypress', function(e) {
     }
 })
 
+window.addEventListener('load', function(e) {
+    createLabel()
+})
+
+function createParagraph() {
+    const p = document.createElement('p')
+    return p
+}  
+
+function createLabel() {
+    for (let i in palavraFrase) {
+        const p = createParagraph()
+        p.classList.add(`p-${i}`)
+        p.textContent = palavraFrase[i]
+        divParagrafo.appendChild(p)
+    }
+}
+
 function checkWord() {
-   inputPalavra.value = inputPalavra.value.trim()
-    
-    for (let i = 0; i < palavraFrase.length; i++) {
+    inputPalavra.value = inputPalavra.value.trim()
+
+    for (let i in palavraFrase) {
         if (inputPalavra.value === palavraFrase[i]) {
             acertos++
             palavraFrase.splice(i, 1)  
-            
             console.log('certo')
             break
         
@@ -35,7 +55,7 @@ function checkWord() {
 
 function checkRes() {
     if (palavraFrase.length == 0) {
-        palavraFrase = frase.split(' ')
+        palavraFrase = frasePrompt.split(' ')
 
         alert(`voce finalizou o teste! Acertos: ${acertos} Errados: ${erros}`)
         
