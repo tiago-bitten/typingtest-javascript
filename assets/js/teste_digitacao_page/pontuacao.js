447d961577b4832b52fd0c3a4ecc42f7c06ffeaa
Tiago Bittencourt <tiago.sbittencourt@icloud.com>
2023-05-29 01:07:49 -0300

    Recomentando classe Pontuacao

--- assets/js/teste_digitacao_page/pontuacao.js
+++ assets/js/teste_digitacao_page/pontuacao.js
@@ -15,38 +15,30 @@ export class Pontuacao {
         const pontuacaoDiv = this.ui.criarDiv(this.ui.container, 'pontuacao', 'modal') // Cria uma div para exibir a pontuação
         const pontuacaoContentDiv = this.ui.criarDiv(pontuacaoDiv, 'modal-content') // Cria uma div para o conteúdo da pontuação
         const span = this.ui.criarSpan('close') // Cria um elemento <span> para fechar a pontuação
-        const p = this.ui.criarP() // Cria um elemento <p> para exibir a pontuação
-        const botao = this.ui.criarBotao('botao-reniciar-teste', 'Reniciar')
+        const p = this.ui.criarP(); // Cria um elemento <p> para exibir a pontuação
+        const botao = this.ui.criarBotao('botao-reniciar-teste', 'Reniciar') // Cria um botão para reiniciar o teste
 
         span.innerHTML = '&times;' // Define o conteúdo do <span> como '&times;' para exibir um símbolo de fechar
-        span.onclick = () => pontuacaoDiv.remove(); // Adiciona um evento de clique para remover a div de pontuação ao clicar no <span>
-        botao.onclick = () => window.location.reload()
+        span.onclick = () => pontuacaoDiv.remove() // Adiciona um evento de clique para remover a div de pontuação ao clicar no <span>
+        botao.onclick = () => window.location.reload() // Adiciona um evento de clique para recarregar a página ao clicar no botão "Reiniciar"
 
-        p.innerHTML = `PPM: ${this.getPPM} </br>
-                       ACC: ${this.getACC}%` // Define o conteúdo do <p> como a pontuação atual (PPM e ACC)
+        p.innerHTML = `PPM: ${this.getPPM} </br> ACC: ${this.getACC}%` // Define o conteúdo do <p> como a pontuação atual (PPM e ACC)
         
         pontuacaoContentDiv.appendChild(span) // Adiciona o <span> à div de conteúdo da pontuação
         pontuacaoContentDiv.appendChild(p) // Adiciona o <p> à div de conteúdo da pontuação
-        pontuacaoContentDiv.appendChild(botao)
-
+        pontuacaoContentDiv.appendChild(botao) // Adiciona o botão "Reiniciar" à div de conteúdo da pontuação
 
         pontuacaoDiv.style.display = "block" // Define o estilo da div de pontuação para exibi-la na tela
     }
-      
 
     get getPPM() {
-        const minutos = this.tempo.getSegundos / 60 // Calcula o número de minutos usando os segundos do objeto Tempo
-
+        const minutos = this.tempo.getSegundos / 60; // Calcula o número de minutos usando os segundos do objeto Tempo
         this.ppm = Math.round(this.testeDigitacao.tamanhoTeste / minutos) // Calcula os caracteres por minuto dividindo o tamanho do teste pelo número de minutos
-        // A função Math.round() é usada para arredondar o valor para o número inteiro mais próximo
-
         return this.ppm // Retorna a pontuação de caracteres por minuto
     }
 
     get getACC() {
-        this.acc = Math.round(((this.getAcertos - this.getErros) / this.testeDigitacao.tamanhoTeste) * 100) // Calcula a precisão (accuracy) dividindo a diferença entre acertos e erros pelo tamanho do teste e multiplicando por 100
-        // A função Math.round() é usada para arredondar o valor para o número inteiro mais próximo
-
+        this.acc = Math.round(((this.getAcertos - this.getErros) / this.testeDigitacao.tamanhoTeste) * 100); // Calcula a precisão (accuracy) dividindo a diferença entre acertos e erros pelo tamanho do teste e multiplicando por 100
         return this.acc // Retorna a pontuação de precisão
     }
 
