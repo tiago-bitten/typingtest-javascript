b574967db1eaa6a3c56803e153abc3f355f0f2b7
Tiago Bittencourt <tiago.sbittencourt@icloud.com>
2023-05-29 01:10:48 -0300

    Comentando classe UIHomePage

--- assets/js/home_page/UI_home_page.js
+++ assets/js/home_page/UI_home_page.js
@@ -1,5 +1,6 @@
 export class UIHomePage {
     constructor() {
+        // Seleciona o elemento HTML com a classe 'div-home-buttons' e armazena na propriedade 'divHomeButtons'
         this.divHomeButtons = document.querySelector('.div-home-buttons')
     }
 }
